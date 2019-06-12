f110e860281ab68e79947a95048732038d306ce3
hmj <1004033345@qq.com>
2019-06-12 15:34:52 +0800

    子组件

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <!--子组件-->
+    <div>
+      <div>
+        <h2>我是子组件</h2>
+        <div>
+          <Form>
+            <FormItem>
+              <div>传给父组件</div>
+              <Input v-model="child.msg" placeholder="请输入传递给父组件的数据" @input="changeParentMsg"/>
+              <div>接收父组件过来的值</div>
+              <Input v-model="toChild" placeholder="来自父组件"/>
+            </FormItem>
+          </Form>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:"ChildComponent",
+        data () {
+            return {
+                child:{
+                    msg:""
+                }
+            }
+        },
+        props:{
+            toChild:""
+        },
+        methods:{
+          changeParentMsg(){
+              this.$emit("update:msg",this.child.msg)
+//            this.$emit("msg",this.child.msg)
+          }
+        }
+    }
+</script>
+
+<style>
+
+</style>
