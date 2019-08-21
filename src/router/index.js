90947a7863bb1199c6e9d9d3a66b8a93778a0195
hmj <1004033345@qq.com>
2019-08-21 13:47:41 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,49 @@ const tableSlotScope = () => import('@/pages/TableSlotScope')
 
 const echarts = () =>import('@/pages/Echarts')
 
+const vueUse = () =>import('@/pages/VueUse')
+
+const vueDrag = () =>import('@/pages/VueDrag')
+
+const comment = () =>import('@/pages/Comment')
+
+const testComment = ()=> import('@/pages/testComment')
+
+const optimizationComment = ()=>import('@/pages/OptimizationComment')
+
+const feedBackAndRate = ()=>import('@/pages/FeedBackAndRate')
+
+const store = ()=>import('@/pages/Store')
+
+const lookInfo = () =>import('@/pages/LookInfo')
+
+const lifeCircle = ()=>import('@/pages/LifeCircle')
+
+const functionalComponent = ()=>import('@/pages/FunctionalComponent')
+
+const vueApi = ()=>import('@/pages/VueApi')
+
+const fatherTree = ()=>import('@/pages/FatherTree')
+
+const treeMenu = ()=>import('@/pages/TreeMenu')
+
+const selecte = () =>import('@/pages/Event/SelectEvent')
+
+const robotApi = ()=>import('@/pages/RobotApi')
+
+const barrage = ()=>import('@/pages/Barrage')
+
+const slot = ()=>import('@/pages/Parent')
+
+const brothers = ()=>import('@/pages/Brothers')
+
+const throttle =()=>import('@/pages/Throttle')
+
+const debounce =()=>import('@/pages/Debounce')
+
+const eventAgent =()=>import('@/pages/event/EventAgent')
+
+const render =()=>import('@/pages/Render')
 const router = new Router({
   routes:[
     {
@@ -73,8 +116,148 @@ const router = new Router({
       name:"Echarts",
       title:"图表",
       component:echarts
+    },
+    {
+      path:"/VueUse",
+      name:"VueUse",
+      title:"Vue",
+      component:vueUse
+    },
+    {
+      path:"/VueDrag",
+      name:"VueDrag",
+      title:"拖拽",
+      component:vueDrag
+    },
+    {
+      path:"/Comment",
+      name:"Comment",
+      title:"评论",
+      component:comment
+    },
+    {
+      path:"/testComment",
+      name:"testComment",
+      title:"评论回复",
+      component:testComment
+    },
+    {
+      path:"/OptimizationComment",
+      name:"OptimizationComment",
+      title:"评论优化",
+      component:optimizationComment
+    },
+    {
+      path:"/feedBackAndRate",
+      name:"feedBackAndRate",
+      title:"反馈评星",
+      component:feedBackAndRate
+    },
+    {
+      path:"/Store",
+      name:"Store",
+      title:"Store",
+      component:store
+    },
+    {
+      path:"/LookInfo",
+      name:"LookInfo",
+      title:"查看信息",
+      component:lookInfo
+    },
+    {
+      path:"/LifeCircle",
+      name:"LifeCircle",
+      title:"生命周期",
+      component:lifeCircle
+    },
+    {
+      path:"/FunctionalComponent",
+      name:"FunctionalComponent",
+      title:"函数化组件",
+      component:functionalComponent
+    },
+    {
+      path:"/VueApi",
+      name:"VueApi",
+      title:"vueAPI",
+      component:vueApi
+    },
+    {
+      path:"/FatherTree",
+      name:"FatherTree",
+      title:"递归组件",
+      component:fatherTree
+    },
+    {
+      path:"/TreeMenu",
+      name:"TreeMenu",
+      title:"树菜单",
+      component:treeMenu
+    },
+    {
+      path:"/SelectEvent",
+      name:"SelectEvent",
+      title:"选中事件",
+      component:selecte
+    },
+    {
+      path:"/RobotApi",
+      name:"RobotApi",
+      title:"实用查询",
+      component:robotApi
+    },
+    {
+      path:"/Barrage",
+      name:"Barrage",
+      title:"弹幕",
+      component:barrage
+    },
+    {
+      path:"/Parent",
+      name:"Parent",
+      title:"slot",
+      component:slot
+    },
+    {
+      path:"/Brothers",
+      name:"Brothers",
+      title:"兄弟组件通信",
+      component:brothers
+    },
+    {
+      path:"/Throttle",
+      name:"Throttle",
+      title:"函数节流",
+      component:throttle
+    },
+    {
+      path:"/Debounce",
+      name:"Debounce",
+      title:"函数防抖",
+      component:debounce
+    },
+    {
+      path:"/Event/EventAgent",
+      name:"EventAgent",
+      title:"事件代理/委托",
+      component:eventAgent
+    },
+    {
+      path:"/Render",
+      name:"Render",
+      title:"render",
+      component:render
     }
-  ]
+  ],
+  //滚动
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 300 }
+    }
+  }
 })
 
 export default router
