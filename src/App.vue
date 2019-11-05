3370a5a765ffd6160e937c872cbbfed58b0e6b58
hmj <1004033345@qq.com>
2019-11-05 13:49:01 +0800

    利用sessionStorage实现vuex刷新数据不清空

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,22 @@ export default {
       isRouterActive:true
     }
   },
+  created(){
+    console.log("根页面创建")
+    //解决Vue刷新页面vuex state保存的状态消失问题
+    //页面加载的时候加载sessionStorage或者localStorage里面的状态信息
+    //replaceState 替换 store 的根状态，仅用状态合并或时光旅行调试
+    if(sessionStorage.getItem("store")){
+      let sessionObj = JSON.parse(sessionStorage.getItem("store"))
+      console.log("s---"+JSON.stringify(sessionObj))
+      this.$store.replaceState(Object.assign({},this.$store.state,sessionObj))
+    }
+    //页面刷新时将vuex的状态信息保存到sessionStorage或者localStorage
+    window.addEventListener("beforeunload",()=>{
+      console.log(JSON.stringify(this.$store.state))
+      sessionStorage.setItem("store",JSON.stringify(this.$store.state))
+    })
+  },
   methods:{
     //页面刷新
     reload(){
